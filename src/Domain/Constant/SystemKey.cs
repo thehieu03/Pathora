2a7a6ef8711daf11221e2879fd4fabe2a1f6332b
thehieu03 <hieunthe171211@gmail.com>
2026-02-21 00:30:39 +0700

    Update SystemKey.cs

--- src/Domain/Constant/SystemKey.cs
+++ src/Domain/Constant/SystemKey.cs
@@ -3,9 +3,9 @@ namespace Domain.Constant;
 public sealed class SystemKey : Entity<int>
 {
     public int ParentId { get; set; }
-    public string CodeKey { get; set; }
+    public string CodeKey { get; set; }=null!;
     public int CodeValue { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public int SortOrder { get; set; }
     public bool IsDeleted { get; set; }
 }
