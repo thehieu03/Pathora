3112c7f49c5b020f4c29fe6906ffa083e5d1180a
HuyNQ <ngoquochuyvn2004@gmail.com>
2026-03-12 14:59:12 +0700

    unuse seed

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -5,7 +5,6 @@ using Api.Swagger.Extensions;
 using Application;
 using Infrastructure;
 using Infrastructure.Data;
-using Infrastructure.Data.Seed;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
@@ -23,12 +22,12 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // NOTE: Auto migration/seed disabled for existing database environments.
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await dbContext.Database.MigrateAsync();
-    await AppDbContextSeed.SeedIfNeededAsync(dbContext);
-}
+//await using (var scope = app.Services.CreateAsyncScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    await dbContext.Database.MigrateAsync();
+//    await AppDbContextSeed.SeedIfNeededAsync(dbContext);
+//}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -41,7 +40,7 @@ app.UseExceptionHandler(_ => { });
 app.UseResponseCompression();
 
 app.UseMiddleware<SecurityHeadersMiddleware>();
-app.UseMiddleware<DatabaseAutoSeedMiddleware>();
+//app.UseMiddleware<DatabaseAutoSeedMiddleware>();
 
 app.UseCors();
 app.UseMiddleware<LanguageResolutionMiddleware>();
